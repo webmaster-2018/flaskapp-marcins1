e7e1c7a2cac74e61f5847724fedb819aa534c2ef
marcins1 <rondusiowy@gmail.com>
2019-02-16 20:30:55 +0100

    Dodanie widoku edytowania klas

--- uczniowie/views.py
+++ uczniowie/views.py
@@ -46,3 +46,29 @@ def dodajucznia():
         return redirect(url_for('listauczniow'))
 
     return render_template('dodajucznia.html', form=form)
+
+def get_or_404(kid):
+    try:
+        k = Klasa.get_by_id(kid)
+        return k
+    except Klasa.DoesNotExist:
+        abort(404)
+
+@app.route("/edycjaklasy")
+def listaklasdoedycji():
+    klasy = Klasa.select()
+    return render_template('listaklasdoedycji.html', klasy=klasy)
+
+@app.route("/edycjaklasy/<int:kid>", methods=['GET', 'POST'])
+def edytujklase(kid):
+  k = get_or_404(kid)
+  form = dKlaseForm()
+
+  if form.validate_on_submit():
+    k.nazwa = form.nazwa.data
+    k.roknaboru = form.roknaboru.data
+    k.rokmatury = form.rokmatury.data
+    k.save()
+    return redirect(url_for('listaklas'))
+
+  return render_template('edytujklase.html', form=form, iducznia=k)
