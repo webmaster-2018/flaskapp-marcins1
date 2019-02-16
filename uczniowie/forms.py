8e94e64b36c7882149c4e5d766d307658da43c51
marcins1 <rondusiowy@gmail.com>
2019-02-16 13:56:51 +0100

    Dodanie formularza dodawania klas oraz widoków dodawania i listy klas

--- uczniowie/forms.py
+++ uczniowie/forms.py
@@ -2,12 +2,15 @@
 # quiz-orm/forms.py
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, HiddenField, FieldList
+from wtforms import StringField, RadioField, HiddenField, FieldList, IntegerField
 from wtforms import SelectField, FormField, BooleanField
 from wtforms.validators import Required
 
 blad1 = 'To pole jest wymagane'
 
 
-class UczenForm(FlaskForm):
-    pass
+class dKlaseForm(FlaskForm):
+    id = HiddenField()
+    nazwa = StringField('Nazwa klasy:', validators=[Required(message=blad1)])
+    roknaboru = IntegerField('Rok naboru:', validators=[Required(message=blad1)])
+    rokmatury = IntegerField('Rok matury:', validators=[Required(message=blad1)])
--- uczniowie/views.py
+++ uczniowie/views.py
@@ -12,3 +12,19 @@ app = Flask(__name__)
 def index():
     """Strona główna"""
     return render_template('index.html')
+
+@app.route("/dodajklase", methods=['GET', 'POST'])
+def dodaj():
+    """Dodawanie klasy"""
+    form = dKlaseForm()
+
+    if form.validate_on_submit():
+        Klasa(nazwa=form.nazwa.data, roknaboru=form.roknaboru.data, rokmatury=form.rokmatury.data).save()
+        return redirect(url_for('index'))
+
+    return render_template('dodajklase.html', form=form)
+
+@app.route("/listaklas")
+def lista():
+    klasy = Klasa.select()
+    return render_template('listaklas.html', klasy=klasy)
